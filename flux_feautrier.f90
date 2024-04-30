0c83622cb9085e059fa1a45b151880fa913304a1
Harrison Cook <hecook@nmsu.edu>
2024-04-30 12:39:19 -0600

    Fixed errors in calculating U
    1. Reduced number of grid points for easy testing.
    2. Commented out unused variables to prevent warnings
    3. Density field now constant
      a. Added a density floor when using the gaussian profie
    4. Coefficient arrays now properly filled at indices (2:nz-1)
    5. Flux array U now properly filled leaving ghost zones untouched

--- flux_feautrier.f90
+++ flux_feautrier.f90
@@ -5,31 +5,31 @@ program flux_feautrier
   implicit none
   
   integer, parameter :: nw=1
-  integer, parameter :: nz=640
+  integer, parameter :: nz=10 ! 640
   integer, parameter :: ng=3
 
   real, dimension(nz) :: z
 
   real, dimension(nz+2*ng,nw) :: U
-  real, dimension(nz,nw) :: V,Ip,Im
+!   real(kind=8), dimension(nz,nw) :: V,Ip,Im
   !real, dimension(nz,nw) :: kappa_H_bf,kappa_H_ff,kappa_Hm_bf,kappa_Hm_ff,kappa_rad
   real, dimension(nz,nw) :: absorp_coeff,source_function,omega
 
-  real, dimension(nz) :: aa,bb,cc,dd
+  real, dimension(nz) :: aa,bb,cc,dd,kappa_p,kappa_m
   
   !real :: alpha_e = 0.6648e-24 ! coefficient
   integer :: overflow_limit
-  real :: float_info_max=3.9085e307
+  real :: float_info_max=3.9085d307
 
-  integer :: greyindex,i,iwave
+  integer :: greyindex,i,iwave,iz
   real :: sigma_sb=5.670374419d-5,sigma_grey=0.4
-  real, dimension(nz) :: T,rho,B_grey,alpha_grey,kappa_grey,omega_grey,dU
+  real, dimension(nz) :: T,rho,B_grey,alpha_grey,kappa_grey,omega_grey!,dU
 
   real, dimension(nw) :: waves_cm,waves_angstrom
   real :: wave_cm,wave_angstrom
 
   real :: zeta,dz,z0,z1
-  real :: H=1.496d14,rho0=1d-9,rho_floor=1d-24
+  real :: rho0=1d-9!,rho_floor=1d-24,H=1.496d14
 
   z0 = -4717816996570149.0
   z1 = 4717816996570149.0
@@ -37,12 +37,13 @@ program flux_feautrier
   do i=1,nz
    z(i) = z0 + (i-1)*dz
    T(i) = 65000.
+   rho(i) = rho0
    ! Implement a desity floor
-   if ( rho0*exp(-.5*z(i)**2/H**2) < 1d-24 ) then
-      rho(i) = 1d-24
-   else
-      rho(i) = rho0*exp(-.5*z(i)**2/H**2)
-   end if ! density floor
+   ! if ( rho0*exp(-.5*z(i)**2/H**2) < 1d-24 ) then
+      ! rho(i) = 1d-24
+   ! else
+      ! rho(i) = rho0*exp(-.5*z(i)**2/H**2)
+   ! end if ! density floor
   enddo ! populate z, T, rho arrays
   
 !   rho=rho0*exp(-.5*z**2/H**2)
@@ -65,26 +66,19 @@ program flux_feautrier
   !theta = 5040./T
   !electron_pressure = ne * kb * T
   !hm_bf_factor = 4.158e-10 * electron_pressure * theta**(5./2) * 10**(0.754 * theta)
-
+  
   greyindex = 0
   B_grey = sigma_sb*T**4
   alpha_grey = 3.68e22 * T**(-3.5) *  rho
   kappa_grey = (alpha_grey+sigma_grey)*rho
   omega_grey = sigma_grey / (alpha_grey + sigma_grey)
 
-   print*, ' T = ', minval(T), maxval(T)
-   print*, ' B_grey = ', minval(B_grey), maxval(B_grey)
-   print*, ' rho = ', minval(rho), maxval(rho)
-   print*, ' sigma_grey = ', sigma_grey
-   print*, ' alpha_grey = ', minval(alpha_grey), maxval(alpha_grey)
-   print*, ' kappa_grey = ', minval(kappa_grey), maxval(kappa_grey)
-   print*, ' omega_grey = ', minval(omega_grey), maxval(omega_grey)
-
+  ! Loop over wavelength
   do iwave=1,nw
      wave_cm = waves_cm(iwave)
      wave_angstrom = waves_angstrom(iwave)
 
-
+     
      !damping_factor = h*c/wave_cm/kb/T
      !do i=1,nz
      !   if (damping_factor(i) > overflow_limit) then
@@ -95,7 +89,7 @@ program flux_feautrier
      
      !chi = 1.2398e4 / wave_angstrom
      !stim_factor = 1-10**(-chi*theta)
-
+     
      !
      ! Do grey first 
      !
@@ -104,22 +98,43 @@ program flux_feautrier
      source_function(:,iwave) = B_grey
      omega(:,iwave) = omega_grey
                     
+     ! Populate opacities at point i+1/2
+     do iz=1, nz-1
+       kappa_p(iz) = 0.5 * (absorp_coeff(iz+1,iwave) + absorp_coeff(iz,iwave))
+     enddo
+      
+     ! Populate opacitiets at point i-1/2
+     do iz=2, nz
+       kappa_m(iz) = 0.5 * (absorp_coeff(iz,iwave) + absorp_coeff(iz-1,iwave))
+     enddo
+
+     ! Populate centers of arrays 
+     do iz=2, nz-1
+       aa(iz) = absorp_coeff(iz,iwave)**2 / kappa_m(iz)
+       cc(iz) = absorp_coeff(iz,iwave)**2 / kappa_p(iz)
+       zeta   = dz*dz * absorp_coeff(iz,iwave)**3 * (1 - omega(iz,iwave))
+       bb(iz) = -(aa(iz) + cc(iz) + zeta)
+       dd(iz) = -source_function(iz,iwave) * zeta
+     enddo
+   
+     ! Populate boundary values
      aa(1) = 0
      zeta  = absorp_coeff(1,iwave) * dz**2 * (1 - omega(1,iwave)) / 4
      bb(1) = -(1/absorp_coeff(1,iwave) + dz + zeta) * absorp_coeff(1,iwave)**2
      cc(1) = 1/absorp_coeff(1,iwave) * absorp_coeff(1,iwave)**2
      dd(1) = -source_function(1,iwave) * zeta * absorp_coeff(1,iwave)**2
-
+     
      aa(nz) = 1/absorp_coeff(nz,iwave) * absorp_coeff(nz,iwave)**2
      zeta   = absorp_coeff(nz,iwave) * dz**2 * (1 - omega(nz,iwave)) / 4
      bb(nz) = -(1/absorp_coeff(nz,iwave) + dz + zeta) * absorp_coeff(nz,iwave)**2
      cc(nz) = 0
      dd(nz) = -source_function(nz,iwave) * zeta * absorp_coeff(nz,iwave)**2
-
+ 
 ! solve the system of equations
 
-     call tridag(aa, bb, cc, dd, U(:,iwave))
-     print*,U(:,iwave)
+     call tridag(aa, bb, cc, dd, U(ng+1:nz+(2*ng)-ng,iwave)) ! Write restults to U(:,iwave)
+     print*,'U(:,iwave)='
+     print*, U(:,iwave)
 
      !call update_ghosts(U(:,iwave))
      !call der6(U(:,iwave),dU)
@@ -130,7 +145,7 @@ program flux_feautrier
 
 
 
-  enddo
+  enddo ! wavelength loop
 !
 endprogram flux_feautrier
 !********
