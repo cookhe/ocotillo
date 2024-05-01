81353c55077dfe22a7b738e904aeb9f65febdf69
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-05-01 15:45:46 -0600

    timing the code

--- flux_feautrier.f90
+++ flux_feautrier.f90
@@ -19,7 +19,7 @@ program flux_feautrier
   integer :: overflow_limit
   real :: float_info_max=3.9085d307
 
-  integer :: greyindex,i,iwave,iz
+  integer :: greyindex,i,iw,iz
   real :: sigma_sb=5.670374419d-5,sigma_grey=0.4
   real, dimension(nz) :: T,rho,B_grey,alpha_grey,kappa_grey,omega_grey
 
@@ -27,6 +27,8 @@ program flux_feautrier
   real :: wave_cm,wave_angstrom
 
   real :: zeta,dz,z0,z1,rho0,rho_floor,H
+
+  real :: start, finish
 !
   namelist /input/ z0,z1,rho0,rho_floor,H
 !  
@@ -78,9 +80,12 @@ program flux_feautrier
   omega_grey = sigma_grey / (alpha_grey + sigma_grey)
 
   ! Loop over wavelength
-  wavelength: do iwave=1,nw
-     wave_cm = waves_cm(iwave)
-     wave_angstrom = waves_angstrom(iwave)
+
+  call cpu_time(start)
+
+  wavelength: do iw=1,nw
+     wave_cm = waves_cm(iw)
+     wave_angstrom = waves_angstrom(iw)
 
      
      !damping_factor = h*c/wave_cm/kb/T
@@ -89,7 +94,7 @@ program flux_feautrier
      !      damping_factor(i) = overflow_limit
      !   endif
      !enddo
-     !source_function(:,iwave) = 2*h*c**2/wave_cm**5 * 1/(exp(damping_factor)-1)
+     !source_function(:,iw) = 2*h*c**2/wave_cm**5 * 1/(exp(damping_factor)-1)
      
      !chi = 1.2398e4 / wave_angstrom
      !stim_factor = 1-10**(-chi*theta)
@@ -98,59 +103,66 @@ program flux_feautrier
      ! Do grey first 
      !
      
-     absorp_coeff(:,iwave) = kappa_grey
-     source_function(:,iwave) = B_grey
-     omega(:,iwave) = omega_grey
+     absorp_coeff(:,iw) = kappa_grey
+     source_function(:,iw) = B_grey
+     omega(:,iw) = omega_grey
                     
      ! Populate opacities at point i+1/2
      do iz=1, nz-1
-       kappa_p(iz) = 0.5 * (absorp_coeff(iz+1,iwave) + absorp_coeff(iz,iwave))
+       kappa_p(iz) = 0.5 * (absorp_coeff(iz+1,iw) + absorp_coeff(iz,iw))
      enddo
       
      ! Populate opacitiets at point i-1/2
      do iz=2, nz
-       kappa_m(iz) = 0.5 * (absorp_coeff(iz,iwave) + absorp_coeff(iz-1,iwave))
+       kappa_m(iz) = 0.5 * (absorp_coeff(iz,iw) + absorp_coeff(iz-1,iw))
      enddo
 
      ! Populate centers of arrays 
      do iz=2, nz-1
-       aa(iz) = absorp_coeff(iz,iwave)**2 / kappa_m(iz)
-       cc(iz) = absorp_coeff(iz,iwave)**2 / kappa_p(iz)
-       zeta   = dz*dz * absorp_coeff(iz,iwave)**3 * (1 - omega(iz,iwave))
+       aa(iz) = absorp_coeff(iz,iw)**2 / kappa_m(iz)
+       cc(iz) = absorp_coeff(iz,iw)**2 / kappa_p(iz)
+       zeta   = dz*dz * absorp_coeff(iz,iw)**3 * (1 - omega(iz,iw))
        bb(iz) = -(aa(iz) + cc(iz) + zeta)
-       dd(iz) = -source_function(iz,iwave) * zeta
+       dd(iz) = -source_function(iz,iw) * zeta
      enddo
    
      ! Populate boundary values
      aa(1) = 0.
-     zeta  = absorp_coeff(1,iwave) * dz**2 * (1 - omega(1,iwave)) / 4
-     bb(1) = -(1/absorp_coeff(1,iwave) + dz + zeta) * absorp_coeff(1,iwave)**2
-     cc(1) = 1/absorp_coeff(1,iwave) * absorp_coeff(1,iwave)**2
-     dd(1) = -source_function(1,iwave) * zeta * absorp_coeff(1,iwave)**2
+     zeta  = absorp_coeff(1,iw) * dz**2 * (1 - omega(1,iw)) / 4
+     bb(1) = -(1/absorp_coeff(1,iw) + dz + zeta) * absorp_coeff(1,iw)**2
+     cc(1) = 1/absorp_coeff(1,iw) * absorp_coeff(1,iw)**2
+     dd(1) = -source_function(1,iw) * zeta * absorp_coeff(1,iw)**2
      
-     aa(nz) = 1/absorp_coeff(nz,iwave) * absorp_coeff(nz,iwave)**2
-     zeta   = absorp_coeff(nz,iwave) * dz**2 * (1 - omega(nz,iwave)) / 4
-     bb(nz) = -(1/absorp_coeff(nz,iwave) + dz + zeta) * absorp_coeff(nz,iwave)**2
+     aa(nz) = 1/absorp_coeff(nz,iw) * absorp_coeff(nz,iw)**2
+     zeta   = absorp_coeff(nz,iw) * dz**2 * (1 - omega(nz,iw)) / 4
+     bb(nz) = -(1/absorp_coeff(nz,iw) + dz + zeta) * absorp_coeff(nz,iw)**2
      cc(nz) = 0.
-     dd(nz) = -source_function(nz,iwave) * zeta * absorp_coeff(nz,iwave)**2
+     dd(nz) = -source_function(nz,iw) * zeta * absorp_coeff(nz,iw)**2
  
 ! solve the system of equations
 
-     call tridag(aa, bb, cc, dd, U(n1:n2,iwave))
-     call update_ghosts(U(:,iwave))
-     
-     call der(U(:,iwave),dU)
-     V(:,iwave) = -1/absorp_coeff(:,iwave) * dU / dz
-
-     Ip(:,iwave) = U(n1:n2,iwave) + V(:,iwave)
-     Im(:,iwave) = U(n1:n2,iwave) - V(:,iwave)
+     call tridag(aa, bb, cc, dd, U(n1:n2,iw))
 
   enddo wavelength
 !
+  call cpu_time(finish)
+  print*,"Wall time = ",finish-start," seconds."
+
+  !put this inside a subroutine, like, calculate_auxiliaries
+
+  do iw=1,nw
+    call update_ghosts(U(:,iw))
+    call der(U(:,iw),dU)
+    V(:,iw) = -1/absorp_coeff(:,iw) * dU / dz
+
+    Ip(:,iw) = U(n1:n2,iw) + V(:,iw)
+    Im(:,iw) = U(n1:n2,iw) - V(:,iw)
+  enddo
+!
   open(10,file="intensity.dat",status="replace",action='write')
   do i=1,nz
-    do iwave=1,nw
-      write(unit=10,FMT=*) i,iwave,U(n1+i-1,iwave),V(i,iwave),Ip(i,iwave),Im(i,iwave)
+    do iw=1,nw
+      write(unit=10,FMT=*) i,iw,U(n1+i-1,iw),V(i,iw),Ip(i,iw),Im(i,iw)
     enddo
   enddo
   close(10)
