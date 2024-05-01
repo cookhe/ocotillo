349ba9d168d05422565fe7c5982d390886011a5b
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-05-01 14:51:33 -0600

    identation

--- grid.f90
+++ grid.f90
@@ -1,19 +1,23 @@
 module grid
-implicit none
 
-public
+  implicit none
 
-    ! Number of wavelengths
-    integer, parameter :: nw=1
-    ! Number of grid points in computational domain
-    integer, parameter :: nz=640
+  public
+
+! Number of wavelengths
+  integer, parameter :: nw=1
+
+! Number of grid points in computational domain
+  integer, parameter :: nz=640
     
-    ! For arrays that need ghost zones
-    ! Number of ghost zones
-    integer, parameter :: ng=3
-    ! Number of grid points including ghost zones on each end
-    integer, parameter :: mz=nz+2*ng
-    ! Index locations of fist/last zones of computational domain 
-    integer, parameter :: n1=ng+1,n2=mz-ng
+! For arrays that need ghost zones
+! Number of ghost zones
+  integer, parameter :: ng=3
+
+! Number of grid points including ghost zones on each end
+  integer, parameter :: mz=nz+2*ng
+
+! Index locations of fist/last zones of computational domain 
+  integer, parameter :: n1=ng+1,n2=mz-ng
   
 end module grid
