b6f29512fefb05c664ffcadf4f6a023e64ccda7a
Harrison Cook <hecook@nmsu.edu>
2024-05-06 17:12:20 -0600

    added more constants

--- common.f90
+++ common.f90
@@ -24,11 +24,27 @@ module common
   integer, parameter :: igrey=1
   
 ! Stefan-Boltzmann constant
-  real :: sigma_sb=5.670374419d-5
+  real :: sigma_sb=5.6703744191844314e-05
 !  
   real :: float_info_max=3.9085d307
 
 ! Mass of proton and inverse
-  real:: mp=1.6726231d-24,mp1=1/mp
+  real :: mp=1.67262192369e-24
+  real :: mp1=1 / 1.67262192369e-24
+
+! Mass of electron
+  real :: me=9.1093837015e-28
+
+! Planck constant
+  real :: h_planck=6.62607015e-27
+
+! Pi
+  real :: pi=4*atan(1.0)
+
+! Boltzmann constant in erg/K and eV/K
+  real :: k_cgs=1.380649e-16,k_eV=8.617333262145179e-05
+
+! Ionization potential energy for Hydrogen in eV
+  real :: hydrogen_ionization_eV = 13.6
 !
 endmodule common
