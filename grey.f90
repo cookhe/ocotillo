f55e3e2bc0f9ecb2638886d7297527f34112cbf7
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-14 17:49:12 -0600

    remove alpha from call to subroutine

--- grey.f90
+++ grey.f90
@@ -13,11 +13,12 @@ contains
 !  endfunction
 !******************************************
   subroutine grey_parameters(rho,T,sigma_grey,&
-       B_grey,alpha_grey,kappa_grey,omega_grey)
+       B_grey,kappa_grey,omega_grey)
 
     real, intent(in), dimension(nz) :: rho,T
     real, intent(in) :: sigma_grey
-    real, intent(out), dimension(nz) :: B_grey,alpha_grey,kappa_grey,omega_grey
+    real, dimension(nz) :: alpha_grey
+    real, intent(out), dimension(nz) :: B_grey,kappa_grey,omega_grey
     
     B_grey = sigma_sb*T**4
     alpha_grey = 3.68e22 * T**(-3.5) *  rho
