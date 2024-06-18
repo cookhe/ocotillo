ef6826af114b0fbbd62a65121a12388263b0340c
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-17 22:21:27 -0600

    change from exponentials in constants from e to d

--- source/common.f90
+++ source/common.f90
@@ -23,25 +23,25 @@ module Common
   integer, parameter :: igrey=1
   
 ! Stefan-Boltzmann constant
-  real :: sigma_sb=5.6703744191844314e-05
+  real :: sigma_sb=5.6703744191844314d-05
 !  
   real :: float_info_max=3.9085d307
 
 ! Mass of proton and inverse
-  real :: mp=1.67262192369e-24
-  real :: mp1=1 / 1.67262192369e-24
+  real :: mp=1.67262192369d-24
+  real :: mp1=1 / 1.67262192369d-24
 
 ! Mass of electron
-  real :: me=9.1093837015e-28
+  real :: me=9.1093837015d-28
 
 ! Planck constant
-  real, parameter :: h_planck=6.62607015e-27
+  real, parameter :: h_planck=6.62607015d-27
 
 ! Pi
   real, parameter :: pi=4*atan(1.0)
 
 ! Boltzmann constant in erg/K and eV/K
-  real :: k_cgs=1.380649e-16,k_eV=8.617333262145179e-05
+  real :: k_cgs=1.380649d-16,k_eV=8.617333262145179d-05
 
 ! Ionization potential energy for Hydrogen in eV
   real :: hydrogen_ionization_eV = 13.6
@@ -50,12 +50,12 @@ module Common
 !
   real :: RydbergEnergy = 2.1798741d-11
 !
-  real, parameter :: e_electron_cgs  = 4.8032068e-10 ![esu]: charge
+  real, parameter :: e_electron_cgs  = 4.8032068d-10 ![esu]: charge
 !  
-  real, parameter :: me_cgs = 9.1093897e-28 ! electron mass cgs
+  real, parameter :: me_cgs = 9.1093897d-28 ! electron mass cgs
 !
-  real, parameter :: log10e = log10(exp(1.))
+  real, parameter :: log10e = log10(exp(1d0))
   real, parameter :: Rcm = 2 * pi**2 * me_cgs * e_electron_cgs**4 / (h_planck**3 * c_light_cgs)
-  real, parameter :: Iev = h_planck*c_light_cgs*Rcm * 6.2419e+11 ! convert to eV
+  real, parameter :: Iev = h_planck*c_light_cgs*Rcm * 6.2419d11 ! convert to eV
 !  
 endmodule Common
