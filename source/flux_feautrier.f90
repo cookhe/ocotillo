798c6f57f53dea794ba31f157ef3859c54527e4d
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-19 17:00:11 -0600

    1. add an if statement for reading athena 2. add rho3D from athena output 3. encapsulate rest of code into a xy-loop

--- source/flux_feautrier.f90
+++ source/flux_feautrier.f90
@@ -6,6 +6,7 @@ program flux_feautrier
   use GasState
   use ContinuousOpacity
   use Grey
+  use ReadC
 
   implicit none
 
@@ -15,13 +16,13 @@ program flux_feautrier
   real, dimension(mz) :: z
   real, dimension(nz) :: aa,bb,cc,dd
 
-  integer :: iw
+  integer :: iw,ix,iy
   integer :: log_overflow_limit
 
   real :: sigma_grey
-  real, dimension(nz) :: T,rho
+  real, dimension(nz,ny,nx) :: rho3d
+  real, dimension(nz) :: rho,T
   real, dimension(nz) :: opacity, albedo
-  !real, dimension(nz) :: B_grey,alpha_grey,kappa_grey,omega_grey
   real, dimension(nw) :: waves_cm,waves_angstrom
 
   real, dimension(nz) :: NHII_NHINHII,n,nHII,nHI,ne,ionization_factor
@@ -33,12 +34,12 @@ program flux_feautrier
   real :: start, finish
   real :: w0=3000,w1=5000
 !
-  logical :: lgrey=.false.
+  logical :: lgrey=.false.,lread_athena=.true.
 !
   !real, dimension(nz,nw) :: kappa_H_bf,kappa_H_ff,kappa_Hm_bf,kappa_Hm_ff,kappa_rad
   !real, dimension(nz) :: kappa_p,kappa_m 
 !
-  namelist /input/ z0,z1,w0,w1,sigma_grey,lgrey
+  namelist /input/ z0,z1,w0,w1,sigma_grey,lgrey,lread_athena
 !  
 !  Read the input namelist with the user-defined parameters. 
 !  
@@ -51,62 +52,76 @@ program flux_feautrier
 ! Calculate the grid variables
 !
   call calc_wavelength(w1,w0,waves_angstrom,waves_cm)
-  call calc_grid(z1,z0,z,dz)
-  call calc_density(rho,z)
+  if (.not.lread_athena) then 
+     call calc_grid(z1,z0,z,dz)
+     call calc_density(rho,z)
+  else
+     call read_from_athena(z,dz,rho3d)
+  endif
+
   call calc_temperature(T,z)
-  call calc_hydrogen_ion_frac(rho,T,NHII_NHINHII)
-  call solve_gas_state(rho,NHII_NHINHII,n,nHI,nHII,ne,ionization_factor)
-  call calc_electron_pressure(ne,T,electron_pressure)
-  e_scatter    = get_electron_thomson_scattering(n,nHII)
-  theta        = get_theta(T)
-  hm_bf_factor = get_hydrogen_ion_bound_free(electron_pressure,theta)
 !
 ! Start the counter
 !
-  call cpu_time(start)  
+      call cpu_time(start)    
+!*******************************
+!xy-dependent starts here
+!*******************************
+  xloop: do ix=1,nx
+    yloop: do iy=1,ny
+      if (lread_athena) rho = rho3d(1:nz,iy,ix)
+      call calc_hydrogen_ion_frac(rho,T,NHII_NHINHII)
+      call solve_gas_state(rho,NHII_NHINHII,n,nHI,nHII,ne,ionization_factor)
+      call calc_electron_pressure(ne,T,electron_pressure)
+      e_scatter    = get_electron_thomson_scattering(n,nHII)
+      theta        = get_theta(T)
+      hm_bf_factor = get_hydrogen_ion_bound_free(electron_pressure,theta)
 !
 ! Do a sanity check for grey RT
 !
-  if (lgrey.and.(nw /= 1)) then
-    print*,"For Grey RT use only one wavelength. Switch nw=1 in commons.f90"
-    stop
-  endif
+      if (lgrey.and.(nw /= 1)) then
+         print*,"For Grey RT use only one wavelength. Switch nw=1 in commons.f90"
+         stop
+      endif
 !
 ! Loop over wavelengths
 !
-  wavelength: do iw=1,nw
-    wave_cm = waves_cm(iw)
-    print*, 'waves_cm',waves_cm
-    wave_angstrom = waves_angstrom(iw)
+      wavelength: do iw=1,nw
+         lfirst=(ix==1).and.(iy==1).and.(iw==1) 
+         wave_cm = waves_cm(iw)
+         if (lfirst) print*, 'waves_cm',waves_cm
+         wave_angstrom = waves_angstrom(iw)
 !    
-    if (lgrey) then
-      call grey_parameters(rho,T,sigma_grey,source_function,opacity,albedo)
-    else
-      call calc_source_function(wave_cm,T,source_function,log_overflow_limit) !output: source_function
-      call calc_hydrogen_stimulated_emission(wave_angstrom,theta,stim_factor) !output: stim_factor
-      call calc_opacity_and_albedo(e_scatter,rho,ne,NHII_NHINHII,nHI,nHII,&
-           T,wave_angstrom,hm_bf_factor,stim_factor,ionization_factor,opacity,albedo) ! output: omega, and absorp_coeff
-    endif
+         if (lgrey) then
+            call grey_parameters(rho,T,sigma_grey,source_function,opacity,albedo)
+         else
+            call calc_source_function(wave_cm,T,source_function,log_overflow_limit) !output: source_function
+            call calc_hydrogen_stimulated_emission(wave_angstrom,theta,stim_factor) !output: stim_factor
+            call calc_opacity_and_albedo(e_scatter,rho,ne,NHII_NHINHII,nHI,nHII,&
+                 T,wave_angstrom,hm_bf_factor,stim_factor,ionization_factor,opacity,albedo) ! output: omega, and absorp_coeff
+         endif
 !
 ! Populate coefficient arrays
 !
-    call fill_center_coeffs(aa,bb,cc,dd,opacity,albedo,source_function,dz)
-    call fill_boundary_coeffs(aa,bb,cc,dd,opacity,albedo,source_function,dz)    
+         call fill_center_coeffs(aa,bb,cc,dd,opacity,albedo,source_function,dz)
+         call fill_boundary_coeffs(aa,bb,cc,dd,opacity,albedo,source_function,dz)    
 !
 ! Solve the system of equations
 !
-    call tridag(aa, bb, cc, dd, U(n1:n2,iw))
-    print*, 'min/max(U)', minval(U(n1:n2,iw)), maxval(U(n1:n2,iw))
+         call tridag(aa, bb, cc, dd, U(n1:n2,iw))
+         if (lfirst) print*, 'min/max(U)', minval(U(n1:n2,iw)), maxval(U(n1:n2,iw))
 !
-    absorp_coeff(:,iw)=opacity
+         absorp_coeff(:,iw)=opacity
 !
- enddo wavelength
+      enddo wavelength
+    enddo yloop
+  enddo xloop
 !
 ! Finish the counter and print the wall time
 !
   call cpu_time(finish)
   print*,"Wall time = ",finish-start," seconds."
-  print*,"Execution time =",(finish-start)/(nz*nw)*1e6,&
+  print*,"Execution time =",(finish-start)/(nz*ny*nx*nw)*1e6,&
        " micro-seconds per frequency point per mesh point." 
 !
 ! Calculate post-processing quantities: flux and intensities. 
