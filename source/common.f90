d8dffd28c72d1e759da0b311338bd97b321ac28a
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-21 01:41:27 -0600

    add Iev1

--- source/common.f90
+++ source/common.f90
@@ -58,6 +58,7 @@ module Common
   real, parameter :: log10e = log10(exp(1d0))
   real, parameter :: Rcm = 2 * pi**2 * me_cgs * e_electron_cgs**4 / (h_planck**3 * c_light_cgs)
   real, parameter :: Iev = h_planck*c_light_cgs*Rcm * 6.2419d11 ! convert to eV
+  real :: Iev1 = 1./Iev
 !  
   logical :: lfirst
 !
