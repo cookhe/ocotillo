73a8dc3451a1b985f1538007a2624ba2b437ed05
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-24 17:22:20 -0600

    update the script

--- samples/multiInTest/a.sh
+++ samples/multiInTest/a.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
+
+# Check if the executable exists
+if [ ! -f ./source/flux_feautrier.x ]; then 
+    echo "You need to compile and run the code"
+    echo "before running this script."
+fi
+
+# Save the original input so it is not rewritten
+if [ ! -f input_orig.in ]; then
+    cp input.in input_orig.in
+fi
+    
 nsnap=1
 
-if ($nsnap > 10)
-   nzeros='000'
-if ($nsnap >= 10 and $nsnap < 100 )
-   nzeros='00'
-if if ($nsnap >= 100 and $nsnap < 1000 )
-   nzeros='0'
-if ($nsnap >= 1000
-   nzeros=''
-   
 for ((n=0; n <= $nsnap; n++)); 
-do    
+do
+
+    if [ $n > 10 ]; then
+	nzeros='000'
+    elif [ $n >= 10 && $n < 100 ]; then
+	nzeros='00'
+    elif [ $n >= 100 && $n < 1000 ]; then
+	nzeros='0'
+    elif [ $n >= 1000 ]; then    
+	nzeros=''
+    else
+	echo "too many snapshots."
+    fi
+
+    # Replace the snapshot line in the input file
     sed "s/snapshot=.*/snapshot='$nzeros$n'/" input_orig.in > input.in
     if [ ! -d output ]; then
 	mkdir output
     fi
+
     ./source/flux_feautrier.x
+    
+# Rewrite the output snapshot if it already exists
     datadir="output_snapshot"$nzeros$n
     if [ -d $datadir ]; then
 	rm -rf $datadir
