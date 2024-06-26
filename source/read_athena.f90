d637104d3f87f54ecdaa0db314ad94394b0df5c4
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-26 10:13:06 -0600

    change the read so it constructs a serial z but maintains the original partition in x and y

--- source/read_athena.f90
+++ source/read_athena.f90
@@ -17,10 +17,12 @@ module ReadAthena
   real :: Mbh_SolarMasses,r0ref_rg
   real :: aspect_ratio,mean_molecular_weight,rho0
 
-  integer :: nproc=1
+  integer :: nprocz=2,nprocy=1,nprocx=2
+  integer :: iprocx=0,iprocy=0
   
   namelist /athena_input/ RunName,Mbh_SolarMasses,r0ref_rg,&
-       aspect_ratio,mean_molecular_weight,rho0,nproc,datadir,snapshot
+       aspect_ratio,mean_molecular_weight,rho0,datadir,snapshot,&
+       nprocz,nprocy,nprocx,iprocx,iprocy
 
   
 contains
@@ -28,7 +30,7 @@ contains
     subroutine read_from_athena(z,dz,rho,temp)
 !
       integer(c_int) :: coordsys
-      integer(c_int) :: nxloc,nyloc,nzloc
+      integer(c_int) :: nxloc_,nyloc_,nzloc
       integer(c_int) :: nvar,nscalars
       integer(c_int) :: selfgrav_boolean, particles_boolean
       real(c_double) :: gamma1,cs
@@ -37,18 +39,17 @@ contains
       real(c_double), allocatable :: xloc(:),yloc(:),zloc(:)
       real(c_double), allocatable :: tmp_loc(:,:,:)
 !
-      real, dimension(nz,ny,nx), intent(out) :: rho,temp
+      real, dimension(nz,nyloc,nxloc), intent(out) :: rho,temp
 !
       real, dimension(mz), intent(out) :: z
       real, dimension(nz) :: zn      
       real, intent(out) :: dz
-      integer :: iproc
+      integer :: iproc,iprocz
       character(len=90)   :: head,tail,filename,sproc,base
 !
-      real :: dx,dy
-      real :: x0,y0,z0
-      integer :: ix0,iy0,iz0
-      integer :: ix1,iy1,iz1
+      real :: z0
+      integer :: iz0
+      integer :: iz1
 
   !order: 
   !coordsys,nx,ny,nz,nvar,nscalars,selfgrav_boolean, particles_boolean,gamma1,cs,t,dt,x,y,z,rho,rux,ruy,ruz,eng
@@ -59,8 +60,11 @@ contains
 
       base=trim(datadir)//"/id"
       tail="."//trim(snapshot)//".bin"
-      
-      do iproc=0,nproc-1
+
+      do iprocz=0,nprocz-1
+
+        iproc = modulo(iprocz,nprocz) * nprocx*nprocy + modulo(iprocy,nprocy) * nprocx + modulo(iprocx,nprocx)
+
         sproc=itoa(iproc)
         head=trim(base)//trim(sproc)//"/"//trim(RunName)
         if (iproc==0) then 
@@ -78,57 +82,40 @@ contains
         open(99, file = trim(FileName), form = 'unformatted', ACCESS='stream')
 
         read(99) coordsys
-        read(99) nxloc,nyloc,nzloc,nvar,nscalars
+        read(99) nxloc_,nyloc_,nzloc,nvar,nscalars
         read(99) selfgrav_boolean, particles_boolean
         read(99) gamma1,cs
         read(99) t,dt
 
-        allocate(xloc(nxloc)); read(99) xloc
-        allocate(yloc(nyloc)); read(99) yloc
+        allocate(xloc(nxloc_)); read(99) xloc
+        allocate(yloc(nyloc_)); read(99) yloc
         allocate(zloc(nzloc)); read(99) zloc
-        if (nproc==1) zn=zloc
 !
-        if (iproc==0) then
-            x0=xloc(1)
-            y0=yloc(1)
+        if (iprocz==0) then
             z0=zloc(1)
-            dx=xloc(2)-xloc(1)
-            dy=yloc(2)-yloc(1)
             dz=zloc(2)-zloc(1)
          endif
 
-        ix0 = nint((xloc(1)-x0)/dx) + 1
-        ix1 = ix0 + nxloc - 1
-
-        iy0 = nint((yloc(1)-y0)/dy) + 1
-        iy1 = iy0 + nyloc - 1
-
         iz0 = nint((zloc(1)-z0)/dz) + 1
         iz1 = iz0 + nzloc - 1
-!
-! Not all processors need to do this,
-! only if that slice of z hasn't been
-! allocated yet.         
 !
         zn(iz0:iz1) = zloc
 !
 ! Sanity check
 !
-        if (iproc==nproc-1) then 
-          if (ix1 /= nx) then
-            print*,"Resolution in x from Athena = ",ix1
-            print*,"RT x-dimensionality from resolution.in = ",nx
-            print*,"The Athena x dimensionality does not match the chosen for the RT post-processing. Fix."
-            stop
-          endif
-!
-          if (iy1 /= ny) then
-            print*,"Resolution in y from Athena = ",iy1
-            print*,"RT y-dimensionality from resolution.in = ",ny
-            print*,"The Athena y dimensionality does not match the chosen for the RT post-processing. Fix."
-            stop
-          endif
-!
+        if (nxloc_ /= nxloc) then
+          print*,"Resolution in x from Athena = ",nxloc_
+          print*,"RT x-dimensionality from resolution.in = ",nxloc
+          print*,"The Athena x dimensionality does not match the chosen for the RT post-processing. Fix."
+          stop
+        endif
+        if (nyloc_ /= nyloc) then
+          print*,"Resolution in y from Athena = ",nyloc_
+          print*,"RT y-dimensionality from resolution.in = ",nyloc
+          print*,"The Athena y dimensionality does not match the chosen for the RT post-processing. Fix."
+          stop
+        endif
+        if (iprocz==nprocz-1) then
           if (iz1 /= nz) then
             print*,"Resolution in z from Athena = ",iz1
             print*,"RT z-dimensionality from resolution.in = ",nz
@@ -137,7 +124,7 @@ contains
           endif
         endif
 !
-        allocate(tmp_loc(nzloc,nyloc,nxloc)) 
+        allocate(tmp_loc(nzloc,nyloc_,nxloc_))
 !
         !rho1=1./rho(:,iy,ix)
         !ekin = 0.5*rho1*ru2(:,iy,ix)
@@ -146,23 +133,23 @@ contains
         !temp(:,iy,ix) = mean_molecular_weight * amu * cs2 * gamma_inv * k1_cgs
 
         read(99) tmp_loc
-        rho(iz0:iz1,iy0:iy1,ix0:ix1) = tmp_loc
+        rho(iz0:iz1,:,:) = tmp_loc
 !
 ! Build kinetic energy; ekin = (rux**2+ruy**2+ruz**2)/(2*rho)
 !        
         read(99) tmp_loc !rux
-        temp(iz0:iz1,iy0:iy1,ix0:ix1) = .5*tmp_loc**2/rho(iz0:iz1,iy0:iy1,ix0:ix1)
+        temp(iz0:iz1,:,:) = .5*tmp_loc**2/rho(iz0:iz1,:,:)
         read(99) tmp_loc !ruy
-        temp(iz0:iz1,iy0:iy1,ix0:ix1) = temp(iz0:iz1,iy0:iy1,ix0:ix1) + .5*tmp_loc**2/rho(iz0:iz1,iy0:iy1,ix0:ix1)
+        temp(iz0:iz1,:,:) = temp(iz0:iz1,:,:) + .5*tmp_loc**2/rho(iz0:iz1,:,:)
         read(99) tmp_loc !ruz
-        temp(iz0:iz1,iy0:iy1,ix0:ix1) = temp(iz0:iz1,iy0:iy1,ix0:ix1) + .5*tmp_loc**2/rho(iz0:iz1,iy0:iy1,ix0:ix1)
+        temp(iz0:iz1,:,:) = temp(iz0:iz1,:,:) + .5*tmp_loc**2/rho(iz0:iz1,:,:)
 !        
 ! Define internal energy; eint = eng - ekin.
 ! Then the sound speed; cs2 = gamma*gamma1 * eint * rho1
 ! The gammas and the constants will be added in the next subroutine 
 !        
         read(99) tmp_loc !eng
-        temp(iz0:iz1,iy0:iy1,ix0:ix1) = (tmp_loc-temp(iz0:iz1,iy0:iy1,ix0:ix1))/rho(iz0:iz1,iy0:iy1,ix0:ix1)
+        temp(iz0:iz1,:,:) = (tmp_loc-temp(iz0:iz1,:,:))/rho(iz0:iz1,:,:)
         
         deallocate(tmp_loc,xloc,yloc,zloc)
         
@@ -177,7 +164,7 @@ contains
 !************************************************************************************
     subroutine postprocess_athena_values(rho,temp,zn,z,dz,gamma1)
 !
-      real, dimension(nz,ny,nx), intent(inout) :: rho, temp
+      real, dimension(nz,nyloc,nxloc), intent(inout) :: rho, temp
       real, dimension(mz), intent(out) :: z
       real, dimension(nz) :: zn
       real, intent(in) :: gamma1
@@ -215,7 +202,7 @@ contains
         z(n2+i) = z(n2) + i*dz
       enddo
 !
-      do ix=1,nx; do iy=1,ny
+      do ix=1,nxloc; do iy=1,nyloc
         call calc_temperature(temp(:,iy,ix),z,lfrom_read_athena=.true.)
       enddo; enddo
 !
@@ -226,9 +213,9 @@ contains
       print*,'unit_temperature = ',unit_temperature,' K'
 !
       print*,'minval(rho),maxval(rho),mean(rho)',&
-           minval(rho),maxval(rho),sum(rho)/(nx*ny*nz)
+           minval(rho),maxval(rho),sum(rho)/(nxloc*nyloc*nz)
       print*,'minval(T),maxval(T),mean(T)',&
-           minval(temp),maxval(temp),sum(temp)/(nx*ny*nz)
+           minval(temp),maxval(temp),sum(temp)/(nxloc*nyloc*nz)
 !
     endsubroutine postprocess_athena_values
 !************************************************************************************
