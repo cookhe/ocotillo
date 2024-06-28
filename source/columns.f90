a0acb17a58691095b1d5a52cd8e9ea87721a5ab5
Wladimir Lyra <wladimir.lyra@gmail.com>
2024-06-28 15:07:57 -0600

    no need for the labels, only the declarations

--- source/columns.f90
+++ source/columns.f90
@@ -6,7 +6,6 @@ module Columns
 
   public
   
-  integer, parameter :: ncolumns=22
   type column_case
      real, dimension (nz) :: rho,rho1,T,T1,NHII_NHINHII
      real, dimension (nz) :: number_density,inv_number_density,nHI,nHII,ne,ionization_factor
@@ -14,29 +13,5 @@ module Columns
      real, dimension (nz) :: hm_bf_factor,stim_factor
      real, dimension (nz) :: source_function,albedo,opacity
   endtype column_case
-  character (len=18), parameter, dimension(ncolumns) :: column_names = (/ &
-       'rho               ', &
-       'rho1              ', &
-       'T                 ', &
-       'T1                ', &
-       'NHII_NHINHII      ', &
-       'number_density    ', &
-       'inv_number_density', &
-       'nHI               ', &
-       'nHII              ', &
-       'ne                ', &
-       'ionization_factor ', &
-       'electron_pressure ', &
-       'e_scatter         ', &
-       'theta             ', &
-       'theta1            ', &
-       'lgtheta           ', &
-       'lgtheta2          ', &
-       'hm_bf_factor      ', &
-       'stim_factor       ', &
-       'source_function   ', &
-       'opacity           ', & 
-       'albedo            '  & 
-       /)
 
 endmodule Columns
